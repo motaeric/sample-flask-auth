31f30f2e78352e8f781037dd002e837dca92ccfc
motaeric <motaeric@icloud.com>
2026-04-01 14:27:01 -0300

    atualizando

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
 db = SQLAlchemy(app)
 
-
-
 @app.route("/Hello-world", methods=["GET"])
 def hello_world():
     return "Hello, World!"
--- models/user.py
+++ models/user.py
@@ -1,9 +1,8 @@
 from app import db
+from flask_login import UserMixin
 
-class User(db.model):
+class User(db.model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
 
-    def __repr__(self):
-        return f"<User {self.username}>"
